888c58a768dc61e137e8c6352e923622b230f9e4
dszopinski <damian.szopinski@verestro.com>
2023-10-21 13:42:34 +0200

    Add css support.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod window;
 use adw::gio;
 use adw::prelude::*;
 use adw::{Application, glib};
+use adw::gdk::Display;
+use gtk::CssProvider;
 use crate::window::Window;
 
 static APP_ID: &str = "org.shiishiji.Integration1";
@@ -13,10 +15,23 @@ fn main() -> glib::ExitCode {
 
     let app = Application::builder().application_id(APP_ID).build();
 
+    app.connect_startup(|_| load_css());
     app.connect_activate(build_ui);
 
     // Run the application
     app.run()
+}
+
+fn load_css() {
+    let provider = CssProvider::new();
+    provider.load_from_resource("/org/shiishiji/integration1/style.css");
+
+    // Add the provider to the default screen
+    gtk::style_context_add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
 
 fn build_ui(app: &Application) {
